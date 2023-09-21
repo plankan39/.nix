6a9a705ec531db6ad521f739b2ff52717e66ad81
Lukas Elmlund <lukas.elmlund@gmail.com>
2023-09-21 13:06:34 +0200

    added neovim

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -47,6 +47,7 @@
       spotify
       kitty
       vscode
+      gnumake
     ];
 
     sessionVariables = {
---
+++ home-manager/neovim/default.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+
+let
+  vimLua = lua: ''
+    lua << EOF
+    ${lua}
+    EOF
+  '';
+in {
+  home.packages = with pkgs; [ neovide graphviz ];
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+}
+
+
