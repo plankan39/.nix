20d11c71264d048973f303ffdd94aa005b16760e
Lukas Elmlund <lukas.elmlund@gmail.com>
2023-10-05 12:06:26 +0200

    background

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -7,7 +7,16 @@
       "window.titleBarStyle" = "custom";
       "editor.lineHeight" = 1.6;
       "editor.fontSize" = 12;
+      "workbench.colorTheme" = "Catppuccin Macchiato";
     };
+    extensions = with pkgs.vscode-extensions; [
+      catppuccin.catppuccin-vsc-icons
+      catppuccin.catppuccin-vsc
+      usernamehw.errorlens
+      ms-python.isort
+      ms-python.python
+      njpwerner.autodocstring
+    ];
   };
 
 }
